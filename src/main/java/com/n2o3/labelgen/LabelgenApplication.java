75835385f6c28dccff6c0455a13ac51ca72ed893
Patryk <patryklkakol@gmail.com>
2019-04-07 16:34:00 +0200

    init

---
+++ src/main/java/com/n2o3/labelgen/LabelgenApplication.java
@@ -0,0 +1,13 @@
+package com.n2o3.labelgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabelgenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabelgenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/n2o3/labelgen/LabelgenApplicationTests.java
@@ -0,0 +1,16 @@
+package com.n2o3.labelgen;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LabelgenApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
